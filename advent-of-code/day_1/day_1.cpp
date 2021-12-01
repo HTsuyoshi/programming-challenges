27b79a2de7fe25fbc444880f1c93eb14f8c44362
HTsuyoshiy <henriquetsuyoshiy@gmail.com>
2021-12-01 20:53:18 -0300

    refact(day 1): open input from a file

--- advent-of-code/day_1/day_1.cpp
+++ advent-of-code/day_1/day_1.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
+#include<fstream>
 
 using namespace std;
 
-int day_1(int n)
+int day_1(int n, fstream& input)
 {
     int *values = new int[n],
         larger = 0,
@@ -11,16 +12,15 @@ int day_1(int n)
 
     while (i < n)
     {
-        cin >> tmp;
+        input >> tmp;
         values[i] = tmp;
         i++;
     }
 
     i %= n;
-    while (cin >> tmp)
+    while (input >> tmp)
     {
         if (values[i] < tmp) larger++;
-        cout << values[i] << ' ' << tmp << endl;
         values[i] = tmp;
         i++;
         i %= n;
@@ -30,8 +30,15 @@ int day_1(int n)
 }
 
 int main() {
-    int increase_time = day_1(3);
-    cout << "The depth increase " << increase_time << " times." << endl;
+    fstream input("./input_1.txt", ios::in);
+    int increase_time = day_1(1, input);
+    cout << "Task 1: The depth increase " << increase_time << " times." << endl;
+    input.close();
+
+    input.open("./input_2.txt", ios::in);
+    increase_time = day_1(3, input);
+    cout << "Task 2: The depth increase " << increase_time << " times." << endl;
+    input.close();
     return 0;
 }
 
